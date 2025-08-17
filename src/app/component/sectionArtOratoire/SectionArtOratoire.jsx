74c1999bc318e58225092e21deb3b5d0d9f26c33
Ali <zallitoaliinfo@gmail.com>
2025-08-18 01:33:26 +0200

    responsive

--- src/app/component/sectionArtOratoire/SectionArtOratoire.jsx
+++ src/app/component/sectionArtOratoire/SectionArtOratoire.jsx
@@ -9,19 +9,19 @@ export default function SectionArtOratoire() {
         <h2 className="text-3xl pb-10">Art Oratoire</h2>
         <p className="w-3/5 pl-20 text-xl">Découvrez l'art de captiver votre audience et de transmettre vos idées avec impact. Nos ateliers d'art oratoire vous accompagnent dans le développement de vos compétences de communication, pour que chaque prise de parole devienne une opportunité de briller et d'inspirer.</p>
         <div className="flex md:flex-row flex-col justify-center items-center w-3/4 flex-wrap gap-10 mt-10 ml-10 font-semibold">
-        <div className="flex lg:flex-row flex-col  justify-around items-center w-4/10 bg-amber-50 p-6 rounded-2xl">
+        <div className="flex lg:flex-row flex-col  justify-around items-center md:w-4/10 w-3/4 bg-amber-50 p-6 rounded-2xl">
           <i class="bi bi-chat py-3 px-4 text-xl text-white rounded-xl bg-emerald-900"></i>
           <p>Maîtrise de l'expression orale et corporelle</p>
         </div>
-        <div className="flex lg:flex-row flex-col justify-around items-center w-4/10 bg-amber-50 p-6 rounded-2xl">
+        <div className="flex lg:flex-row flex-col justify-around items-center md:w-4/10 w-3/4 bg-amber-50 p-6 rounded-2xl">
           <i class="bi bi-people py-3 px-4 text-xl text-white rounded-xl bg-emerald-900"></i>
           <p>Gestion du stress et de l'anxiété en public</p>
         </div>
-        <div className="flex lg:flex-row flex-col justify-around items-center w-4/10 bg-amber-50 p-6 rounded-2xl">
+        <div className="flex lg:flex-row flex-col justify-around items-center md:w-4/10 w-3/4 bg-amber-50 p-6 rounded-2xl">
           <i class="bi bi-bullseye py-3 px-4 text-xl text-white rounded-xl bg-emerald-900"></i>
           <p>Structuration claire et impactante des idées</p>
         </div>
-        <div className="flex lg:flex-row flex-col justify-around items-center w-4/10 bg-amber-50 p-6 rounded-2xl">
+        <div className="flex lg:flex-row flex-col justify-around items-center md:w-4/10 w-3/4 bg-amber-50 p-6 rounded-2xl">
           <i class="bi bi-lightbulb py-3 px-4 text-xl text-white rounded-xl bg-emerald-900"></i>
           <p>Développement de la confiance en soi</p>
         </div>
--- src/app/component/sectionContact/SectionContact.jsx
+++ src/app/component/sectionContact/SectionContact.jsx
@@ -7,21 +7,21 @@ export default function SectionContact() {
     <>
       <footer id="contact" className="pb-5 h-[50vh] border-t-1 border-black ">
 
-        <div className="flex justify-evenly items-center w-4/5 mx-auto mt-10 p-10">
-          <h2 className="text-xl bg-white p-5 rounded-2xl shadow-lg font-semibold">Rejoins-nous !</h2>
-          <h2 className="text-xl bg-white p-5 rounded-2xl shadow-lg font-semibold"> <a href="https://www.paypal.com/be/home?locale.x=fr_BE">Soutient-nous</a></h2>
-          <h2 className="text-xl bg-white p-5 rounded-2xl shadow-lg font-semibold"><a href="/contact">Contactes-nous</a></h2>
+        <div className="lg:text-xl flex-col md:flex-row gap-10 text-lg flex justify-evenly items-center w-4/5 mx-auto mt-10 p-10">
+          <h2 className=" bg-white p-5 rounded-2xl shadow-lg font-semibold">Rejoins-nous !</h2>
+          <h2 className="bg-white p-5 rounded-2xl shadow-lg font-semibold"> <a href="https://www.paypal.com/be/home?locale.x=fr_BE">Soutiens-nous</a></h2>
+          <h2 className="bg-white p-5 rounded-2xl shadow-lg font-semibold"><a href="/contact">Contacte-nous</a></h2>
 
         </div>
 
         <div className="flex justify-center items-center gap-10 text-white text-xl">
           <a href="https://www.facebook.com/">
-          <i class="bi bi-facebook p-5 rounded-full bg-emerald-900"></i></a>
-          
+            <i class="bi bi-facebook p-5 rounded-full bg-emerald-900"></i></a>
+
           <a href="https://www.instagram.com/">
-          <i class="bi bi-instagram p-5 rounded-full bg-emerald-900"></i></a>
+            <i class="bi bi-instagram p-5 rounded-full bg-emerald-900"></i></a>
           <a href="https://www.linkedin.com/">
-          <i class="bi bi-linkedin p-5 rounded-full bg-emerald-900"></i></a>
+            <i class="bi bi-linkedin p-5 rounded-full bg-emerald-900"></i></a>
         </div>
 
         <div>
